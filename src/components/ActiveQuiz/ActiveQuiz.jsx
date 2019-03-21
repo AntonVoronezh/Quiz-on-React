6b8de3bf5f31a17256e1f8c337fdaec4659e7989
AntonVoronezh <pstut@mail.ru>
2019-03-21 19:16:11 +0300

    added activequiz

--- src/components/ActiveQuiz/ActiveQuiz.jsx
+++ src/components/ActiveQuiz/ActiveQuiz.jsx
@@ -4,11 +4,18 @@ import classes from "./ActiveQuiz.css";
 const ActiveQuiz = props => {
   return (
     <div className={classes.ActiveQuiz}>
-      Вопрос
       <span className={classes.Question}>
         <strong>Где смысл?</strong>
         <small>1 из 10</small>
       </span>
+      <div>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ul>
+      </div>
     </div>
   );
 };
