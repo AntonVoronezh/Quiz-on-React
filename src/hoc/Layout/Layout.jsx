cd2814882842ea320e142f0546e6e5a5b65364df
AntonVoronezh <pstut@mail.ru>
2019-03-22 21:27:51 +0300

    4

--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 import classes from "./Layout.css";
 import Quiz from "../../containers/Quiz/Quiz.jsx";
+import MenuToggle from "../../components/Navigation/MenuToggle/MenuToggle.jsx";
 
 class Layout extends Component {
+  state = { menu: false };
+
+  toggleMenuHandler = () => {
+    this.setState({ menu: !this.state.menu });
+  };
+
   render() {
     return (
       <div className={classes.Layout}>
         <main>
+          <MenuToggle
+            onToggle={this.toggleMenuHandler}
+            isOpen={this.state.menu}
+          />
           <Quiz />
         </main>
       </div>
